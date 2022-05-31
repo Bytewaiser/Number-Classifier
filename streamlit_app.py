aa457b22e7bc4eb4cf8688e404005a68aba83ba7
Bytewaiser <bytewaiser@gmail.com>
2022-05-31 21:42:24 +0300

    Checking for None types

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,16 +30,21 @@ with col_1:
     )
 
 
-a = canvas_result.image_data[:,:,:-1]
-a = Image.fromarray(a).resize((28, 28))
-a = np.array(a).mean(axis=2).astype(np.uint8)
-arr = a.reshape(1, 28, 28, 1) / 255
+a = canvas_result.image_data
+if a is None:
+    st.write("Wait for app to be load")
+else:
+    a = a[:,:,:-1]
+    a = Image.fromarray(a).resize((28, 28))
+    a = np.array(a).mean(axis=2).astype(np.uint8)
+    arr = a.reshape(1, 28, 28, 1) / 255
+    
+    p = model.predict(arr, verbose=0)
+    
+    with col_2:
+        st.bar_chart(p.T)
+    
+    st.write("Your number is ", p.argmax())
 
-p = model.predict(arr, verbose=0)
-
-with col_2:
-    st.bar_chart(p.T)
-
-st.write("Your number is ", p.argmax())
 st.write("Note: Only drawback for this app is drawings is not the same resolution with the traninig data")
 st.write("Because of that reason predictions may not be good")
